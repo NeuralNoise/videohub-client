b12f479d3ef9385a133251cdbbc440324406bb7d
Mike Parent <mparent@umich.edu>
2016-03-01 12:19:15 -0600

    Fix tests by configuring DJES

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ author = "Onion Tech Team"
 author_email = "tech@theonion.com"
 license = "BSD"
 requires = [
-    "Django>=1.7.1",
+    "Django>=1.7.1,<1.9",
     "django-betty-cropper>=0.1.9",
     "djes>=0.1.108",
 ]
